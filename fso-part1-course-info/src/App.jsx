dabcb9b2b9175b5599810a2c4ad1f161d341b12b
amandaw800 <amanda.v.wei@gmail.com>
2026-05-23 16:43:17 -0400

    Finish course information exercise 1.1

---
+++ fso-part1-course-info/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = ({course}) => {
+  return (
+    <>
+      <h1>{course}</h1>
+    </>
+  );
+}
+
+const Content = ({parts, exercises}) => {
+
+  return (
+    <>
+      <p>
+        {parts} {exercises}
+      </p>
+    </>
+
+  );
+
+}
+
+const Total = ({exercise1, exercise2, exercise3}) => {
+  return (
+    <>
+      <p>
+        {exercise1 + exercise2 + exercise3}
+
+      </p>
+    </>
+  );
+  
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={part1} exercises={exercises1}/>
+      <Content parts={part2} exercises={exercises2}/>
+      <Content parts={part3} exercises={exercises3}/>
+      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3}/>
+    </div>
+  )
+}
+
+export default App
